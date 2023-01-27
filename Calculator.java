414a89a3af4f99882b88c4e4b1fd2c875225db4d
Hamidul Islam <hamidulasm@gmail>
2023-01-27 20:15:51 +0530

    Calculator

---
+++ Calculator.java
@@ -0,0 +1,17 @@
+class Calculator{
+	static double add(double number1, double number2) {
+		return number1 + number2;
+	}
+	
+	static double subtract(double number1, double number2) {
+		return number1 - number2;
+	}
+	
+	static double multiply(double number1, double number2) {
+		return number1 * number2;
+	}
+	
+	static double divide(double number1, double number2) {
+		return number1 / number2;
+	}
+}
